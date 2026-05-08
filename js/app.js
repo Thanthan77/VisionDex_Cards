928e21e564966603a79d21564a4692ffdff200dd
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-08 15:48:24 -0400

    feat:  ajout bouton generer carte aléatoire

--- js/app.js
+++ js/app.js
@@ -34,3 +34,25 @@ document.getElementById("generate").addEventListener("click", async () => {
   // Redirection vers la page carte
   window.location.href = "assets/carte.html?data=" + encoded;
 });
+
+document.getElementById("random-card").addEventListener("click", async () => {
+  const DESC_WORKER = "https://late-flower-c74a.ethanqc-chea.workers.dev";
+  const CARD_WORKER = "https://orange-forest-580d.ethanqc-chea.workers.dev";
+
+  //Générer une description aléatoire
+  const descRes = await fetch(DESC_WORKER);
+  const { description } = await descRes.json();
+
+  //Générer la carte complète
+  const cardRes = await fetch(CARD_WORKER, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ description }),
+  });
+
+  const cardData = await cardRes.json();
+
+  //Redirection vers carte.html
+  const encoded = encodeURIComponent(JSON.stringify(cardData));
+  window.location.href = "assets/carte.html?data=" + encoded;
+});
